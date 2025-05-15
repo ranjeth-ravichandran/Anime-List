43651331b5ec0b6ea0de1213607fde77ea2127e4
Ranjeth <ranjeth1965@gmail.com>
2025-05-15 16:08:14 +0100

    changed styling, added new API, added top anime and manga

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ANIME LIST - R
+- Create a users json with temp data
+- Load user data onto the site and style & design the page
+  - Username
+  - Profile Picture
+  - Anime / Manga on screen
+  - User Rating
+  - Anime Data
+    - Rating
+    - Title
+    - Image
+    - Introduction
+    - Trailer?
+
+## API Usage
+- [Jiken API](https://jikan.moe/) -> Anime & Manga
+- [AniList API](https://docs.anilist.co/guide) -> User data
+
+By reading the documentation, to get the top recommended anime the GET request is:
+```
+https://api.jikan.moe/v4/top/anime?page=1&limit=10
+```
--- main.js
+++ main.js
@@ -1,52 +1,77 @@
+/* fetch('data/users.json')
+    .then(response => response.json())
+    .then(users => {
+        const userContainer = document.getElementById('userContainer');
+        const userInfo = document.getElementById('userInfo');
 
+        users.users.forEach(user => {
+            const userProfile = document.createElement('div');
+            userProfile.classList.add('user-profile');
+            userProfile.innerHTML = `
+                        <img src="https://via.placeholder.com/100" alt="User Avatar">
+                        <p>${user.username}</p>
+                    `;
+            userProfile.addEventListener('click', () => {
+                userInfo.style.display = 'block';
+                userInfo.innerHTML = `
+                            <h2>${user.username}</h2>
+                            <p><strong>Email:</strong> ${user.email}</p>
+                            <p><strong>Favorite Genres:</strong> ${user.favorite_genres.join(', ')}</p>
+                            <h3>Watchlist:</h3>
+                            <ul>
+                                ${user.watchlist.map(anime => `<li>${anime.title} - ${anime.status}</li>`).join('')}
+                            </ul>
+                        `;
+            });
+            userContainer.appendChild(userProfile);
+        });
+    })
+    .catch(error => console.error('Error loading users:', error)); */
 
-const colorPallet = document.querySelector('.color-pallets');
-const greenPallet = ['#F1F5D8', '#D6FFC1', '#B9FFAF', '#97FA9A', '#8AF0BF'];
-const bluePallet = ['#EFEFEF', '#B1E6F3', '#72DDF7', '#79B8F4', '#8093F1'];
-const tealPallet = ['#006767', '#008080', '#279E9D', '#4EBCBA', '#75DAD7'].reverse();
-const redPallet = ['#EC5353', '#EE7272', '#F09191', '#F2B0B0', '#F3CFCE'].reverse();
-const brownPallet = ['#E7C6A2', '#D3B392', '#BFA081', '#BF9675', '#966548'];
-const yellowPallet = ['#FFD000', '#F9DC5C', '#FAE588', '#FCEFB4', '#FDF8E1'].reverse();
-const orangePallet = ['#FD9A4D', '#FCB274', '#FBCA9A', '#FCDFAA', '#FCF3B9'].reverse();
+/* TODO: Fetching the Top 10 Anime  */
+/* GET https://api.jikan.moe/v4/top/anime?page=1&limit=10 */
 
+fetch("https://api.jikan.moe/v4/top/anime?page=1&limit=15")
+    .then((response) => response.json())
+    .then((json) => {
+        const animeListData = json.data;
+        console.log(animeListData)
+        console.log(animeListData.length)
 
-colorPallet.innerHTML = `
-    <h2>Color Pallets</h2>
-    <h3>Green</h3>
-    <ul>
-        ${greenPallet.map(color => `<li style="background-color: ${color};" data-color="${color}">${color}</li>`).join('')}
-    </ul>
-    <h3>Blue</h3>
-    <ul>
-        ${bluePallet.map(color => `<li style="background-color: ${color};" data-color="${color}">${color}</li>`).join('')}
-    </ul>
-    <h3>Teal</h3>
-    <ul>
-        ${tealPallet.map(color => `<li style="background-color: ${color};" data-color="${color}">${color}</li>`).join('')}
-    </ul>
-    <h3>Red</h3>
-    <ul>
-        ${redPallet.map(color => `<li style="background-color: ${color};" data-color="${color}">${color}</li>`).join('')}
-    </ul>
-    <h3>Brown</h3>
-    <ul>
-        ${brownPallet.map(color => `<li style="background-color: ${color};" data-color="${color}">${color}</li>`).join('')}
-    </ul>
-    <h3>Yellow</h3>
-    <ul>
-        ${yellowPallet.map(color => `<li style="background-color: ${color};" data-color="${color}">${color}</li>`).join('')}
-    </ul>
-    <h3>Orange</h3>
-    <ul>
-        ${orangePallet.map(color => `<li style="background-color: ${color};" data-color="${color}">${color}</li>`).join('')}
-    </ul>
-`;
+        animeListData.map((anime) => {
 
-document.querySelectorAll('.color-pallets li').forEach(item => {
-    item.addEventListener('click', function () {
-        const color = this.dataset.color;
-        navigator.clipboard.writeText(color).then(() => {
-            alert(`Copied: ${color}`);
-        }).catch(err => console.error('Failed to copy text: ', err));
+            const animeList = document.getElementById('anime-list');
+            let animeContainer = document.createElement('div');
+            animeContainer.classList.add('anime');
+            animeList.appendChild(animeContainer)
+            animeContainer.innerHTML = `
+                <a href="${anime.url}">
+                    <img src="${anime.images.jpg.image_url}">
+                </a>
+            `
+        })
     });
-});
+
+
+/* TODO: Fetching the Top 10 Manga  */
+/* GET https://api.jikan.moe/v4/top/manga?page=1&limit=10 */
+fetch("https://api.jikan.moe/v4/top/manga?page=1&limit=15")
+    .then((response) => response.json())
+    .then((json) => {
+        const mangaListData = json.data;
+        console.log(mangaListData)
+        console.log(mangaListData.length)
+
+        mangaListData.map((manga) => {
+
+            const mangaList = document.getElementById('manga-list');
+            let mangaContainer = document.createElement('div');
+            mangaContainer.classList.add('anime');
+            mangaList.appendChild(mangaContainer)
+            mangaContainer.innerHTML = `
+                <a href="${manga.url}">
+                    <img src="${manga.images.jpg.image_url}">
+                </a>
+            `
+        })
+    });
